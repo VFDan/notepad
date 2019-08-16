c0391db9a125cc5853a21900c25100a055dc0e87
VFDan <44413950+VFDan@users.noreply.github.com>
2019-08-16 18:31:51 -0400

    Update script.js
    Remove onkeyup, gonna use in html

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
 document.getElementById('textarea').value = localStorage.getItem('content');
-document.getElementById('textarea').onkeyup = function(){localStorage.setItem('content', document.getElementById('textarea').value))};

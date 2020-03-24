eaa4aaf9ce4cd795a58c3d3e04fb62938c6ade87
VFDan <44413950+VFDan@users.noreply.github.com>
2020-03-24 14:51:00 -0400

    Start to add support for dark mode
    + Sets dark mode to false if not set already
    + Adds class to body if dark mode is active

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.getElementById('textarea').value = localStorage.getItem('content');
+if (typeof(localStorage.getItem('dark')) === "undefined") {
+  localStorage.setItem('dark', false);
+}
+
+if (localStorage.getItem('dark')) {
+  document.body.className += ' ' + 'dark';
+} else {
+  document.body.className += ' ' + 'light';
+}
